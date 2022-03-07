f468be33ddc920d7ede0177ce71b86d14fcf9a57
Jacquestill <jacquestill9@icloud.com>
2022-03-07 20:24:54 +0900

    delete note function added

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -12,12 +12,27 @@ function App() {
       return [...prevValue, newNote];
     });
   }
+
+  function deleteNote(id) {
+    setArea((prevValue) => {
+      return prevValue.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
       <CreateArea onAdd={addNote} />
       {area.map((showNotes, index) => (
-        <Note key={index} title={showNotes.title} content={showNotes.content} />
+        <Note
+          key={index}
+          id={index}
+          title={showNotes.title}
+          content={showNotes.content}
+          onDelete={deleteNote}
+        />
       ))}
       <Footer />
     </div>
